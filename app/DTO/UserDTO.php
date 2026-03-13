7714b1a790ebac62080b879b13b47654a4c6464d
ShodanMaster <akshaykrishnan922@gmail.com>
2026-03-13 09:51:27 +0530

    Create Request and DTO

---
+++ app/DTO/UserDTO.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DTO;
+
+class UserDTO
+{
+    public $name;
+    public $email;
+
+    public function __construct($name,$email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public static function fromRequest($request)
+    {
+        return new self(
+            $request->name,
+            $request->email
+        );
+    }
+}
