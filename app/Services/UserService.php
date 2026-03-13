6f2aca478700237ca039cfedc860e78189f00618
ShodanMaster <akshaykrishnan922@gmail.com>
2026-03-13 09:56:42 +0530

    Create Repository and Service

---
+++ app/Services/UserService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services;
+
+use App\DTO\UserDTO;
+use App\Repositories\UserRepository;
+
+class UserService
+{
+    protected $repo;
+
+    public function __construct(UserRepository $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    public function createUser(UserDTO $dto)
+    {
+        return $this->repo->create([
+            'name' => $dto->name,
+            'email' => $dto->email
+        ]);
+    }
+}
